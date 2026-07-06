77ee734f541dc89764ec090688c9d66e9e3db418
seecret1 <naintonight@gmail.com>
2026-07-06 23:43:13 +0300

    clear code

--- order-service/src/main/java/com/github/seecret1/order_service/exception/OrderNotFound.java
+++ order-service/src/main/java/com/github/seecret1/order_service/exception/OrderNotFound.java
@@ -2,9 +2,9 @@ package com.github.seecret1.order_service.exception;
 
 public class OrderNotFound extends RuntimeException {
 
-  public OrderNotFound(String message) {
+    public OrderNotFound(String message) {
       super(message);
-  }
+    }
 
-  public OrderNotFound() { }
+    public OrderNotFound() { }
 }
