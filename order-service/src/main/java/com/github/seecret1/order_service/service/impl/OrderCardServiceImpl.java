02474aca4cf56657c6fca7d9020bdd2be9f67ad8
seecret1 <naintonight@gmail.com>
2026-07-21 00:43:48 +0300

    update logic in pending order

--- order-service/src/main/java/com/github/seecret1/order_service/service/impl/OrderCardServiceImpl.java
+++ order-service/src/main/java/com/github/seecret1/order_service/service/impl/OrderCardServiceImpl.java
@@ -3,6 +3,9 @@ package com.github.seecret1.order_service.service.impl;
 import com.github.seecret1.order_service.dto.OrderMessage;
 import com.github.seecret1.order_service.dto.card.OrderCardDto;
 import com.github.seecret1.order_service.entity.OrderCard;
+import com.github.seecret1.order_service.entity.OrderType;
+import com.github.seecret1.order_service.exception.OrderTypeException;
+import com.github.seecret1.order_service.feign.UserServiceClient;
 import com.github.seecret1.order_service.kafka.OrderKafkaProducerService;
 import com.github.seecret1.order_service.mapper.OrderCardManualMapper;
 import com.github.seecret1.order_service.repository.OrderCardRepository;
@@ -26,6 +29,25 @@ public class OrderCardServiceImpl implements OrderCardService {
     public OrderMessage createOrder(OrderCardDto event) {
 
         // TODO: Добавить обработку и работу со статусами
+        if (event.getOrderType() == OrderType.CARD) {
+            event.validate();
+        }
+        else throw new OrderTypeException("Order card service works only with OrderType=CARD");
+
+        // TODO: добавить проверку счетов карт (если имеется задолженность -> REJECT)
+
+        // TODO: если:
+        //  тип полчения = в офисe (возможно:
+        //  1) что нет офисов в данном городе (REJECT)
+        //  2) офисы закрыты на какое-то время (REJECT)
+        //  3) нужно сначала осуществить доставку карты в офис [при type=DEBIT_PROFILE])
+        //  тип полечния = доставка -> правизываем работу с delivery-service
+        //  Примеры:
+        //  Работа с delivery-service: (возможно доставка по данному адресу не осуществляется в данный момент)
+        //  Возможно доставка в целом недоступна из-за нагрузки
+
+        // TODO: продумать логику со статусом ERROR
+
         OrderCard order = OrderCardManualMapper.toEntity(event);
         orderCardRepository.save(order);
 
