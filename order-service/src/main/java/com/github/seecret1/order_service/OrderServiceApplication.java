04e567f99ab21f7ddfe203310d87cc197133affa
seecret1 <naintonight@gmail.com>
2026-07-22 23:51:43 +0300

    update kafka config and add ssl settings

--- order-service/src/main/java/com/github/seecret1/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/github/seecret1/order_service/OrderServiceApplication.java
@@ -1,9 +1,20 @@
 package com.github.seecret1.order_service;
 
+import com.github.seecret1.order_service.feign.UserServiceClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan(basePackages = {
+		"com.github.seecret1.common",
+		"com.github.seecret1.jwt_common.security",
+		"com.github.seecret1.order_service"
+})
+@EnableFeignClients(basePackageClasses = {UserServiceClient.class})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
