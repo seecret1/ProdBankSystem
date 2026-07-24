9b5c690645c40d0eef85ae52c0431db0bee3ff0d
seecret1 <naintonight@gmail.com>
2026-07-24 21:31:50 +0300

    init office service

---
+++ office-service/src/main/java/com/github/seecret1/office_service/OfficeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.seecret1.office_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeServiceApplication.class, args);
+	}
+
+}
